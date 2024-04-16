faf56d5fee40bf12747072012e1b71d0dde9cdc7
nayon117 <hasibul.nayon1@gmail.com>
2024-04-16 14:56:01 +0600

    create filter ui

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,6 +1,7 @@
 import Filter from "@/components/shared/Filter";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
 
 const Home = () => {
@@ -26,7 +27,11 @@ const Home = () => {
           otherClasses='flex-1'
         />
 
-        <Filter />
+        <Filter 
+        filters={HomePageFilters}
+        otherClasses="min-h-[56px] sm:min-w-[170px]"
+        containerClasses="hidden max-md:flex"
+        />
        
       </div>
     </>
