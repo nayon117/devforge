55ea9261b1af02a9c273604d7fd47b5734089631
nayon117 <hasibul.nayon1@gmail.com>
2024-04-16 07:22:11 +0600

    create basic homepage

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,12 +1,27 @@
-// import Navbar from "@/components/shared/navbar/Navbar";
-// import { UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Home = () => {
-    return(
-        <div>
-             {/* <UserButton afterSignOutUrl="/" /> */}
-             {/* <Navbar/> */}
-        </div>
-    )
-}
+  return (
+    <>
+      <div className="flex w-full flex-row-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Link href="/ask-question" className="flex justify-end max-sm:w-full">
+          <Button className="primary-gradient min-h-[48px] px-4 py-3 !text-light-900">
+            {" "}
+            Ask a Question{" "}
+          </Button>
+        </Link>
+      </div>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        {/* <LocalSearchbar></LocalSearchbar> */}
+
+        {/* <Filters></Filters> */}
+
+      </div>
+    </>
+  );
+};
 export default Home;
