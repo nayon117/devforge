4fc26471e939544d6448cf7da9621a395c60fedc
nayon117 <hasibul.nayon1@gmail.com>
2024-04-17 11:44:40 +0600

    make the form reusable

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -3,7 +3,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Editor } from "@tinymce/tinymce-react";
 
 import {
@@ -21,8 +21,12 @@ import { questionsSchema } from "@/lib/validations";
 import { Badge } from "../ui/badge";
 import Image from "next/image";
 
+const type:any = "create";
+
 const Question = () => {
   const editorRef = useRef(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
   // 1. Define your form.
   const form = useForm<z.infer<typeof questionsSchema>>({
     resolver: zodResolver(questionsSchema),
@@ -35,7 +39,14 @@ const Question = () => {
 
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof questionsSchema>) {
-    console.log(values);
+    setIsSubmitting(true);
+    try {
+      // make a async call to the server
+    } catch (error) {
+      // handle error
+    }finally{
+      setIsSubmitting(false)
+    }
   }
 
   //   onclick handler for tag
@@ -46,33 +57,32 @@ const Question = () => {
   ) => {
     if (e.key === "Enter" && field.name === "tags") {
       e.preventDefault();
-      const tagInput =  e.target as HTMLInputElement;
+      const tagInput = e.target as HTMLInputElement;
       const tagValue = tagInput.value.trim();
 
-      if(tagValue !== ''){
-        
-        if(tagValue.length>15){
-            return form.setError("tags", {
-                type:"required",
-                message:"Tag should be less than 15 characters"
-            })
+      if (tagValue !== "") {
+        if (tagValue.length > 15) {
+          return form.setError("tags", {
+            type: "required",
+            message: "Tag should be less than 15 characters",
+          });
         }
 
-        if(!field.value.includes(tagValue as never)){
-            form.setValue("tags",[...field.value,tagValue])
-            tagInput.value = ""
-            form.clearErrors("tags")
+        if (!field.value.includes(tagValue as never)) {
+          form.setValue("tags", [...field.value, tagValue]);
+          tagInput.value = "";
+          form.clearErrors("tags");
         }
-      }else{
-        form.trigger()
+      } else {
+        form.trigger();
       }
     }
   };
 
-  const handleTagRemove = (tag:string,field:any) =>{
-       const newTags= field.value.filter((t:string)=> t !==tag)
-       form.setValue('tags', newTags)
-  }
+  const handleTagRemove = (tag: string, field: any) => {
+    const newTags = field.value.filter((t: string) => t !== tag);
+    form.setValue("tags", newTags);
+  };
 
   return (
     <Form {...form}>
@@ -165,27 +175,31 @@ const Question = () => {
               </FormLabel>
               <FormControl className="mt-3.5">
                 <>
-                <Input
-                  className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
-                  placeholder="Add tags ..."
-                  onKeyDown={(e) => handleInputKeyDown(e, field)}
-                />
-                {field.value.length>0 &&(
+                  <Input
+                    className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
+                    placeholder="Add tags ..."
+                    onKeyDown={(e) => handleInputKeyDown(e, field)}
+                  />
+                  {field.value.length > 0 && (
                     <div className="flex-start mt-2.5 gap-2.5">
-                        {field.value.map((tag:any)=>(
-                            <Badge key={tag} className="subtle-medium background-light800_dark300  text-light400_light500 flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize" onClick={()=>handleTagRemove(tag,field)}>
-                                {tag}
-                                <Image 
-                                src='/assets/icons/close.svg'
-                                alt="close"
-                                width={12}
-                                height={12}
-                                className="cursor-pointer object-contain invert-0 dark:invert"
-                                />
-                            </Badge>
-                        ))}
+                      {field.value.map((tag: any) => (
+                        <Badge
+                          key={tag}
+                          className="subtle-medium background-light800_dark300  text-light400_light500 flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize"
+                          onClick={() => handleTagRemove(tag, field)}
+                        >
+                          {tag}
+                          <Image
+                            src="/assets/icons/close.svg"
+                            alt="close"
+                            width={12}
+                            height={12}
+                            className="cursor-pointer object-contain invert-0 dark:invert"
+                          />
+                        </Badge>
+                      ))}
                     </div>
-                )}
+                  )}
                 </>
               </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
@@ -196,7 +210,17 @@ const Question = () => {
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        <Button
+          type="submit"
+          className="primary-gradient w-fit !text-light-900"
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? (
+            <>{type === "edit" ? "Editing..." : "Posting..."}</>
+          ) : (
+            <>{type === "edit" ? "Edit Question" : "Ask a Question"}</>
+          )}
+        </Button>
       </form>
     </Form>
   );
