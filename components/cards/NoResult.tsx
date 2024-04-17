a3adfffac302fbe0e0e6451ff70a401b1e9ff8cb
nayon117 <hasibul.nayon1@gmail.com>
2024-04-17 16:38:07 +0600

    create interface for no result component

--- components/cards/NoResult.tsx
+++ components/cards/NoResult.tsx
@@ -1,5 +1,13 @@
 
-const NoResult = () => {
+interface Props {
+    title: string;
+    description: string;
+    link: string;
+    linkTitle: string;
+  
+}
+
+const NoResult = ({title,description, link, linkTitle}:Props) => {
     return(
         <div>
              <p> Welcome to NoResult part </p>
