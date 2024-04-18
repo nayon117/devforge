d0e668e168eb31447eea72aee68160885259d9c1
nayon117 <hasibul.nayon1@gmail.com>
2024-04-18 06:09:36 +0600

    create webhook and user

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,6 +2,7 @@
 
 import User from "../models/user.model";
 import { connectToDatabase } from "../mongoose"
+import { CreateUserParams } from "./shared.types";
 
 
 export async function getUserById(params:any) {
@@ -14,5 +15,18 @@ export async function getUserById(params:any) {
 
     } catch (error) {
         console.log(error);
+        throw error
+    }
+}
+
+// CREATE USER
+export async function createUser(userData:CreateUserParams) {
+    try {
+        connectToDatabase()
+        const newUser = await User.create(userData)
+        return newUser
+    } catch (error) {
+        console.log(error);
+        throw error
     }
 }
