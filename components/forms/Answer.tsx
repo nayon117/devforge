a2952a6fc7f3014b5f3113a703c0f03534f63aad
nayon117 <hasibul.nayon1@gmail.com>
2024-05-01 07:19:45 +0600

    create answer form

--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -1,9 +1,127 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import React, { useRef, useState } from "react";
+import { Editor } from "@tinymce/tinymce-react";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "../ui/button";
+import { AnswerSchema } from "@/lib/validations";
+import { useTheme } from "@/context/ThemeProvider";
+
+const type: any = "create";
 
 const Answer = () => {
-    return(
-        <div>
-             <p> Welcome to Answer part </p>
-        </div>
-    )
-}
+  const editorRef = useRef(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const { mode } = useTheme();
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof AnswerSchema>>({
+    resolver: zodResolver(AnswerSchema),
+    defaultValues: {
+      answer: "",
+    },
+  });
+
+  const handleCreateAnswer = () =>{
+    setIsSubmitting(true);
+
+    try {
+        // 
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }finally{
+        setIsSubmitting(false);
+    }
+  }
+
+  return (
+    <div className="mt-5">
+      <div className="flex flex-col justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
+        <h4 className="paragraph-semibold text-dark400_light800">
+          Write your answer here
+        </h4>
+        <Button className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500">
+          Generate An AI answer
+        </Button>
+      </div>
+      <Form {...form}>
+        <FormField
+          control={form.control}
+          name="answer"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col gap-3">
+              <FormControl className="mt-3.5">
+                <Editor
+                  apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                  onInit={(_evt, editor) => {
+                    // @ts-ignore
+                    editorRef.current = editor;
+                  }}
+                  onBlur={field.onBlur}
+                  onEditorChange={(content) => field.onChange(content)}
+                  initialValue=""
+                  init={{
+                    height: 350,
+                    menubar: false,
+                    plugins: [
+                      "advlist",
+                      "autolink",
+                      "lists",
+                      "link",
+                      "image",
+                      "charmap",
+                      "preview",
+                      "anchor",
+                      "searchreplace",
+                      "visualblocks",
+                      "codesample",
+                      "fullscreen",
+                      "insertdatetime",
+                      "media",
+                      "table",
+                    ],
+                    toolbar:
+                      "undo redo | blocks | " +
+                      "codesample | bold italic forecolor | alignleft aligncenter " +
+                      "alignright alignjustify | bullist numlist ",
+                    content_style: "body { font-family:Inter; font-size:16px }",
+                  }}
+                />
+              </FormControl>
+              <FormDescription className="body-regular mt-2.5 text-light-500">
+                Include all the information and expand on what you put on the
+                answer.Minimum 20 characters
+              </FormDescription>
+              <FormMessage className="text-red-500" />
+            </FormItem>
+          )}
+        />
+
+        <Button
+          type="submit"
+          className="primary-gradient mt-2.5 w-fit !text-light-900"
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? (
+            <>{type === "edit" ? "Editing..." : "Posting..."}</>
+          ) : (
+            <>{type === "edit" ? "Edit Question" : "Answer the question"}</>
+          )}
+        </Button>
+      </Form>
+    </div>
+  );
+};
 export default Answer;
