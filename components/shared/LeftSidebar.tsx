e84393983286c15b97ddd112d5262a4ffbaa2ba9
nayon117 <hasibul.nayon1@gmail.com>
2024-05-21 19:51:45 +0600

    fix user error

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -13,8 +13,7 @@ const LeftSidebar = () => {
     <section className="background-light900_dark200  light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto  border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
       <div className="flex flex-1 flex-col gap-6">
         {sidebarLinks.map((item) => {
-          const isActive =
-            (pathname.includes(item.route) && item.route.length > 1) || pathname === item.route;
+          const isActive = (pathname.includes(item.route) && item.route.length > 1) || pathname === item.route;
            
             if(item.route === '/profile') {
               if(userId) {
