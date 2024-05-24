3782ad4bf115085986b97766d262c624503db087
nayon117 <hasibul.nayon1@gmail.com>
2024-05-24 16:05:31 +0600

    implement global result comp

---
+++ components/shared/search/GlobalFilters.tsx
@@ -0,0 +1,11 @@
+
+const GlobalFilters = () => {
+    return (
+        <section className=''>
+            <div className=''>
+                Welcome to GlobalFilters
+            </div>
+        </section>
+    )
+}
+export default GlobalFilters;
